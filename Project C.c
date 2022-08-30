2d21bf2beadddae70bcdcd5621141f03610c028d
Gabriel Diaz <101999960+Igwbriel@users.noreply.github.com>
2022-08-29 23:52:51 -0300

    Update Project C.c

--- Project C.c
+++ Project C.c
@@ -74,4 +74,33 @@ void tela_sobre(void) {
     printf("\n");
 }
 
-
+void tela_equipe(void) {
+    printf("\n");
+    printf(".=============================================================================.\n");
+    printf("|                                                                             |\n");
+    printf("|               Universidade Federal do Rio Grande do Norte                   |\n");
+    printf("|                   Centro de Ensino Superior do Seridó                       |\n");
+    printf("|                 Departamento de Computação e Tecnologia                     |\n");
+    printf("|                    Disciplina DCT1106 -- Programação                        |\n");
+    printf("|                 Sistema de Controle de Contas Bancárias                     |\n");
+    printf("|        Developed by @igwbriel and @ericmadsonn -- since august, 2022        |\n");
+    printf("|                                                                             |\n");
+    printf("|=============================================================================|\n");
+    printf("|                                                                             |\n");
+    printf("|         = = = = = Sistema de Controle de Contas Bancárias = = = = =         |\n");
+    printf("|                                                                             |\n");
+    printf("|              This project was developed by:                                 |\n");
+    printf("|                                                                             |\n");
+    printf("|              Gabriel D. Oliveira                                            |\n");
+    printf("|              E-mail: Gabreelzinn@gmail.com                                  |\n");
+    printf("|              Redes sociais: @igwbriel                                       |\n");
+    printf("|              Git: https://github.com/Igwbriel                               |\n");
+    printf("|                                                                             |\n"); 
+    printf("|              Eric M.A.M. De Oliveira                                        |\n");
+    printf("|              E-mail: Ericmadson67@hotmail.com                               |\n");
+    printf("|              Redes sociais: @ericmadsonn                                    |\n");
+    printf("|              Git: https://github.com/ericmadson                             |\n");
+    printf("|                                                                             |\n");
+    printf(".=============================================================================.\n");
+    printf("\n");
+}
