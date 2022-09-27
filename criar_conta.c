60b89175fc3df73823150f0366bafd3a6e43c57a
EricM <ericmadson67@hotmail.com>
2022-09-26 23:04:34 -0300

    a

--- criar_conta.c
+++ criar_conta.c
@@ -8,65 +8,65 @@ void tela_contaCPF(void);
 void tela_contaRG(void);
 void tela_contaConcluida(void);
 
-int criar_conta(void) {
+void criar_conta(void) {
     void tela_contaNome(void) {
-    char op;
-    system("clear||cls");   
-    printf("\n");
-    printf("|=============================================================================|\n");
-    printf("|                                                                             |\n");
-    printf("|                    = = = = = Criação de conta = = = = =                     |\n");
-    printf("|                                                                             |\n");
-    printf("|                          Digite seu nome completo:\n                        |\n");
-    scanf("%c", &op);
-    getchar();
-    printf(".=============================================================================.\n");
-    printf("\n"); 
-}
-// modulo 1 tela 2
-void tela_contaCPF(void) {
-    char op;
-    system("clear||cls");   
-    printf("\n");
-    printf("|=============================================================================|\n");
-    printf("|                                                                             |\n");
-    printf("|                    = = = = = Criação de conta = = = = =                     |\n");
-    printf("|                                                                             |\n");
-    printf("|                          Digite seu CPF:\n                                  |\n");
-    scanf("%c", &op);
-    getchar();
-    printf(".=============================================================================.\n");
-    printf("\n"); 
-}
-// modulo 1 tela 3
-void tela_contaRG(void) {
-    char op;
-    system("clear||cls");   
-    printf("\n");
-    printf("|=============================================================================|\n");
-    printf("|                                                                             |\n");
-    printf("|                    = = = = = Criação de conta = = = = =                     |\n");
-    printf("|                                                                             |\n");
-    printf("|                              Digite seu RG:\n                               |\n");
-    scanf("%c", &op);
-    getchar();
-    printf(".=============================================================================.\n");
-    printf("\n"); 
-}
+        char op;
+        system("clear||cls");   
+        printf("\n");
+        printf("|=============================================================================|\n");
+        printf("|                                                                             |\n");
+        printf("|                    = = = = = Criação de conta = = = = =                     |\n");
+        printf("|                                                                             |\n");
+        printf("|                          Digite seu nome completo:\n                        |\n");
+        scanf("%c", &op);
+        getchar();
+        printf(".=============================================================================.\n");
+        printf("\n"); 
+    }
+ // modulo 1 tela 2
+    void tela_contaCPF(void) {
+        char op;
+        system("clear||cls");   
+        printf("\n");
+        printf("|=============================================================================|\n");
+        printf("|                                                                             |\n");
+        printf("|                    = = = = = Criação de conta = = = = =                     |\n");
+        printf("|                                                                             |\n");
+        printf("|                          Digite seu CPF:\n                                  |\n");
+        scanf("%c", &op);
+        getchar();
+        printf(".=============================================================================.\n");
+        printf("\n"); 
+    }
+ // modulo 1 tela 3
+    void tela_contaRG(void) {
+        char op;
+        system("clear||cls");   
+        printf("\n");
+        printf("|=============================================================================|\n");
+        printf("|                                                                             |\n");
+        printf("|                    = = = = = Criação de conta = = = = =                     |\n");
+        printf("|                                                                             |\n");
+        printf("|                              Digite seu RG:\n                               |\n");
+        scanf("%c", &op);
+        getchar();
+        printf(".=============================================================================.\n");
+        printf("\n"); 
+    }
 // modulo 1 tela final (se funcionar)
-void tela_contaConcluida(void) {
-    char op;
-    system("clear||cls");   
-    printf("\n");
-    printf("|=============================================================================|\n");
-    printf("|                                                                             |\n");
-    printf("|               = = = = = Conta criada com sucesso ! = = = = =                |\n");
-    printf("|                                                                             |\n");
-    printf("|                  [Obrigado por confiar em nossos serviços]\n                |\n");
-    printf("|                                                                             |\n");
-    getchar();
-    printf(".=============================================================================.\n");
-    printf("\n"); 
-}
+    void tela_contaConcluida(void) {
+        char op;
+        system("clear||cls");   
+        printf("\n");
+        printf("|=============================================================================|\n");
+        printf("|                                                                             |\n");
+        printf("|               = = = = = Conta criada com sucesso ! = = = = =                |\n");
+        printf("|                                                                             |\n");
+        printf("|                  [Obrigado por confiar em nossos serviços]\n                |\n");
+        printf("|                                                                             |\n");
+        getchar();
+        printf(".=============================================================================.\n");
+        printf("\n"); 
+    }
 }
 
