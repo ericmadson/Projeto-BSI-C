c56ac0611fc238f07574c4839987a11e6c9a6acd
Gabriel Dias <GabrielDias7820@gmail.com>
2022-09-29 16:19:51 -0300

    resolvendo modulo criar conta

--- criar_conta.c
+++ criar_conta.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <criar_conta.h>
+#include "criar_conta.h"
 
+void tela_cadastro(void) {
+        char nome[100];
+        char cpf[35];
+        char rg[30];
 
-
-void criar_conta(void) {
-   tela_contaNome();
-    tela_contaCPF();
-    tela_contaRG();
-    tela_contaConcluida();
-    return 0;
-}
-
-void tela_contaNome(void) {
-        char op;
         system("clear||cls");   
         printf("\n");
         printf("|=============================================================================|\n");
@@ -22,14 +15,10 @@ void tela_contaNome(void) {
         printf("|                    = = = = = Criação de conta = = = = =                     |\n");
         printf("|                                                                             |\n");
         printf("|                          Digite seu nome completo:\n                        |\n");
-        scanf("%c", &op);
+        scanf("%s", nome);
         getchar();
         printf(".=============================================================================.\n");
         printf("\n"); 
-    }
- // modulo 1 tela 2
-    void tela_contaCPF(void) {
-        char op;
         system("clear||cls");   
         printf("\n");
         printf("|=============================================================================|\n");
@@ -37,14 +26,9 @@ void tela_contaNome(void) {
         printf("|                    = = = = = Criação de conta = = = = =                     |\n");
         printf("|                                                                             |\n");
         printf("|                          Digite seu CPF:\n                                  |\n");
-        scanf("%c", &op);
+        scanf("%s", cpf);
         getchar();
         printf(".=============================================================================.\n");
-        printf("\n"); 
-    }
- // modulo 1 tela 3
-    void tela_contaRG(void) {
-        char op;
         system("clear||cls");   
         printf("\n");
         printf("|=============================================================================|\n");
@@ -52,14 +36,10 @@ void tela_contaNome(void) {
         printf("|                    = = = = = Criação de conta = = = = =                     |\n");
         printf("|                                                                             |\n");
         printf("|                              Digite seu RG:\n                               |\n");
-        scanf("%c", &op);
+        scanf("%s", rg);
         getchar();
         printf(".=============================================================================.\n");
         printf("\n"); 
-    }
-// modulo 1 tela final (se funcionar)
-    void tela_contaConcluida(void) {
-        char op;
         system("clear||cls");   
         printf("\n");
         printf("|=============================================================================|\n");
@@ -71,7 +51,9 @@ void tela_contaNome(void) {
         getchar();
         printf(".=============================================================================.\n");
         printf("\n"); 
-    }
+
+}
+
 //modulo 1 tela final (se não funcionar)
 void tela_contaNao(void) {
     char op;
