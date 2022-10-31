8ad9629ca8e6ab8c98d89a35ae9abbcbaabfb7a1
Gabriel Dias <GabrielDias7820@gmail.com>
2022-10-31 20:11:08 -0300

    origem do struct

--- struct_conta.c
+++ struct_conta.c
@@ -1,5 +1,10 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include "validacao_senha.h"
+#include "criar_conta.h"
 
 int str() {
    
@@ -10,27 +15,43 @@ struct cadastro_conta {
 };
 
 struct cadastro_conta conta;
-    printf("Nome do cliente: \n");
-    fgets(conta.nome, 100,stdin);
-
-    printf("Seu CPF: \n");
-    scanf("%d", &conta.cpf, 15);
 
     printf("crie uma senha de 6 digitos para sua conta: ");
-    scanf("%d", &conta.password, 6);
 
+    printf("|=============================================================================|\n");
+    printf("|                                                                             |\n");
+    printf("|                    = = = = = Criação de conta = = = = =                     |\n");
+    printf("|                                                                             |\n");
+    printf("|                          Digite seu nome completo:\n                        |\n");
+    fgets(conta.nome, 100,stdin);
+    getchar();
+    printf(".=============================================================================.\n");
+    printf("\n");
+    system("clear||cls");   
+    printf("\n");
+    printf("|=============================================================================|\n");
+    printf("|                                                                             |\n");
+    printf("|                    = = = = = Criação de conta = = = = =                     |\n");
+    printf("|                                                                             |\n");
+    printf("|                          Digite seu CPF:\n                                  |\n");
+    scanf("%d", &conta.cpf, 15);
+    getchar();
+    printf(".=============================================================================.\n");
+    system("clear||cls"); 
+    printf("\n");
+    printf("|=============================================================================|\n");
+    printf("|                                                                             |\n");
+    printf("|                    = = = = = Criação de conta = = = = =                     |\n");
+    printf("|                                                                             |\n");
+    printf("|                          Digite sua senha: (apenas números)\n               |\n");
+    scanf("%d", &conta.password, 6);
+    validacao_senha(conta.password);
 
     printf("\n Nome: %s", conta.nome);
     printf("\n Nome: %d", conta.cpf);
     printf("\n Nome: %d", conta.password);
 
-
 //por os "." e "-" no CPF
-
-
-
 
 return 0;
 }
-
-
